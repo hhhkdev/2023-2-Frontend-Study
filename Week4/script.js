e18982d22fc0c177218d79f44f5121d8df4d758e
hhhkdev <c235335@g.hongik.ac.kr>
2023-10-09 18:08:00 +0900

    Week4 clone_codding

---
+++ Week4/script.js
@@ -0,0 +1,94 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const inputText = document.querySelector('.input-text');
+    const acButton = document.querySelector('.reset');
+
+    let currentNumber = '';
+    let previousNumber = '';
+    let currentOperator = '';
+    let operatorJustClicked = false;
+
+    const updateDisplay = () => {
+        inputText.value = currentNumber;
+    };
+
+    const clearData = () => {
+        currentNumber = '';
+        previousNumber = '';
+        currentOperator = '';
+        acButton.inputText = 'AC';
+    };
+
+    const operate = () => {
+        previousNumber = parseFloat(previousNumber); // 어떤 함수인지 알아보기
+        currentNumber = parseFloat(currentNumber);
+
+        switch (currentOperator) {
+            case '+':
+                currentNumber = previousNumber + currentNumber;
+                break;
+            case '-':
+                currentNumber = previousNumber - currentNumber;
+                break;
+            case '×':
+                currentNumber = previousNumber * currentNumber;
+                break;
+            case '÷':
+                currentNumber = previousNumber / currentNumber;
+                break;
+            default:
+                return;
+        }
+        currentOperator = '';
+    };
+
+    document
+        .querySelector('.button-wrapper')
+        .addEventListener('click', function (e) {
+            const buttonValue = e.target.inputText;
+
+            acButton.innerText = 'C'
+
+            if (e.target.matches('.number')) {
+
+                if (operatorJustClicked) {
+                    currentNumber = '';
+                    operatorJustClicked = false;
+                }
+                currentNumber += buttonValue;
+                updateDisplay();
+            } else if (e.target.matches('.operator')) {
+
+                if (currentOperator && currentNumber && previousNumber) {
+                    operate();
+                    updateDisplay();
+                    previousNumber =currentNumber;
+                    currentNumber = '';
+                } else if (!previousNumber) {
+                    previousNumber = currentNumber;
+                    currentNumber = '';
+                }
+
+                if (buttonValue === '=') {
+                    
+                    if (previousNumber && currentOperator) {
+                        operate();
+                        updateDisplay();
+                        previousNumber = '';
+                    }
+                } else if (['+', '-', '×', '÷'].includes(buttonValue)) {
+                    currentOperator = buttonValue;
+                    currentNumber = buttonValue;
+                    updateDisplay();
+                    previousNumber = '';
+                } else if (buttonValue === '.') {
+                    if (!currentNumber.includes('.')) {
+                        currentNumber += '.';
+                        updateDisplay();
+                    }
+                } 
+            } else if (e.target.matches('.reset')) {
+                clearData();
+                updateDisplay();
+            }
+        });
+    })
